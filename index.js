3ca2d6818f9c3d13402bc21398333914ebb5f909
Rob Tice <rob.tice@sonos.com>
2021-07-06 11:57:11 -0400

    IPC implementation with python.

--- index.js
+++ index.js
@@ -10,30 +10,38 @@ app.use(express.static(__dirname + '/public'));
 app.listen(server_port, () => console.log(`App listening on port ${server_port}!`))
 
 /**************************************************************************
- Reads quaternion data from serial port and sends it over the websocket
+ Reads quaternion data from pipe and sends it over the websocket
 **************************************************************************/
 
-const SerialPort = require('serialport')
-const port = new SerialPort('COM12', { baudRate: 115200 })
+const fs              = require('fs');
+const { spawn, fork } = require('child_process');
 
-const Readline = require('@serialport/parser-readline')
-const parser = port.pipe(new Readline({ delimiter: '\n' }))
+const rx_pipe_name = 'quat9_pipe';
+let rx_pipe = spawn('mkfifo', [rx_pipe_name]);  // Create IPC pipe
 
-parser.on('data', function(data) {
-  if (ws != null) {
-     ws.send(data);
-  }
-})
+rx_pipe.on('exit', function(status) {
+  console.log('Created IPC pipe');
+
+  const fd   = fs.openSync(rx_pipe_name, 'r+');
+  let fifoRs = fs.createReadStream(null, { fd });
+  fifoRs.setEncoding('utf8');
+  fifoRs.on('data', data => {
+    console.log('Rx data: ' + data);
+    if (ws != null) {
+      ws.send(data);
+    }
+  });
+});
 
 /**************************************************************************
  Websocket server that communicates with browser
 **************************************************************************/
 
 const WebSocket = require('ws');
- 
+
 const wss = new WebSocket.Server({ port: 8080 });
 var ws = null;
- 
+
 wss.on('connection', function connection(_ws) {
   ws = _ws;
 });
